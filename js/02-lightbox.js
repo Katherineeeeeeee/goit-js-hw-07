f747ddebfc76d44721b02298c9ee600d4b334c7a
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-07-08 14:45:11 +0300

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const makeGallery = () => {
+  const img = galleryItems
+    .map(
+      (el) => `
+      <a class="gallery__item" href="${el.original}">
+      <img class="gallery__image" src="${el.preview}" alt="${el.description}" 
+      title =''/>
+    </a>`
+    )
+    .join("");
+  galleryEl.innerHTML = img;
+};
+
+makeGallery();
+// console.log(galleryItems);
+
+// підключення бібліотеки
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
